48a9e1c44402b30c22fff4583395837d16328e82
gwansiklim <rhkstlr00@gmail.com>
2022-07-04 03:07:59 +0900

    variable basic

---
+++ 1.variable.js
@@ -0,0 +1,17 @@
+/**
+ * 변수(선언하고 정의하는 것):값을 저장하는 공간, 자료를 저장할 수 있는 '이름이 주어진 기억장소'; 
+ * let은 변수에 재할당이 가능하다.; 
+ */
+
+ 
+let a = 0;
+console.log(a);
+
+a = 1;
+console.log(a);
+
+let b;
+console.log(b)
+
+b = 2;
+console.log(b) 
