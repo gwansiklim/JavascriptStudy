c2cb5e53abdc2df55eb2d3d8610d7925e161ef2b
gwansiklim <rhkstlr00@gmail.com>
2022-07-04 03:18:26 +0900

    nameing basic

---
+++ 2.naming.js
@@ -0,0 +1,29 @@
+/** 변수규칙!
+ * 라틴문자(0-9, a-z, A-Z), _
+ * 대소문자를 구분한다.
+ * 추천: camelCase (likeThis)✅
+ * 한국어 ❌
+ * 예약어 ❌
+ * 숫자로 시작 ❌
+ * 특수문자 ❌ (_, $ 두가지는 예외)
+ * 이모지 ❌
+ * 여러개의 변수를 1, 2, 3 숫자ㅣ로 구분 ❌ -> 최대한 의미있게, 구체적인 이름으로 작성하기!!
+*/
+
+let apple;
+let redApple; //camelCase
+
+// 나쁜예제 
+let number = 20;
+let audio1;
+let audio2;
+
+// 좋은 예제
+let myAge = 20;
+let backgroundAudio;
+let windAudio;
+
+
+// 꿀팁!
+let audioBackground;
+let audioWind;
