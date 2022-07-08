c95306c682ded741bf2e6d09155accfb19816cc1
gwansiklim <rhkstlr00@gmail.com>
2022-07-09 02:15:37 +0900

    간단 삼항연산자

---
+++ 3.control/2.ternary.js
@@ -0,0 +1,15 @@
+// 삼항 조건연산자 Ternary Operator
+// 조건식 ? 참인경우 : 거짓인경우
+
+
+let fruit = 'apple';
+if (fruit === 'apple') {
+    console.log('🍎');
+} else {
+    console.log('❌')
+}
+
+fruit === 'apple' ? console.log('🍎') : console.log('❌');
+
+let emoji = fruit === 'apple' ? '🍎' : '❌';
+console.log(emoji);
